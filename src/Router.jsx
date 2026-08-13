dcd8ff351e6867b1be30860bc6ed83719797f669
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-13 15:25:15 +0700

    refactor: added footer and tailwind custom

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from "react-router"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
 
 export default function Router() {
   return (
     <>
     <Routes>
       <Route path={'/'} element={<Navbar/>}/>
-      {/* <Route path={} element={}/> */}
     </Routes>
+    <Footer/>
     </>
   )
 }
