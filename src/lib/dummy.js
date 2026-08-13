19e3f534e9903d0e5f48c36e75e0b0e118c18035
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-13 16:23:08 +0700

    doc: added dummy data

---
+++ src/lib/dummy.js
@@ -0,0 +1,40 @@
+const events = [
+  {
+    id: 1,
+    title: "Go Concurrency Workshop",
+    image:
+      "https://images.unsplash.com/photo-1518770660439-4636190af475",
+    categories: ["Technology", "Programming"],
+    date: "Aug 22, 2026",
+    time: "09:00 WIB",
+    location: "Bandung",
+    attendees: 48,
+    capacity: 100,
+  },
+  {
+    id: 2,
+    title: "React Workshop",
+    image:
+      "https://images.unsplash.com/photo-1633356122544-f134324a6cee",
+    categories: ["Technology", "Programming"],
+    date: "Aug 25, 2026",
+    time: "10:00 WIB",
+    location: "Jakarta",
+    attendees: 75,
+    capacity: 100,
+  },
+  {
+    id: 3,
+    title: "UI/UX Design Meetup",
+    image:
+      "https://images.unsplash.com/photo-1558655146-d09347e92766",
+    categories: ["Design", "UI/UX"],
+    date: "Aug 28, 2026",
+    time: "13:00 WIB",
+    location: "Tangerang",
+    attendees: 32,
+    capacity: 80,
+  },
+];
+
+export default events;
