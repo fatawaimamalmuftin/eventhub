1d1a402f15dd4f25ff8a1311f6e5358ac85e61c4
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-13 12:03:44 +0700

    feat: instalation router

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-
-export default function App() {
-  return (
-    <main>
-      <h1 className="text-red-500">hallo word</h1>
-    </main>
-  )
-}
---
+++ src/Router.jsx
@@ -0,0 +1,12 @@
+import { Routes, Route } from "react-router"
+
+export default function Router() {
+  return (
+    <>
+    <Routes>
+      <Route/>
+      {/* <Route path={} element={}/> */}
+    </Routes>
+    </>
+  )
+}
