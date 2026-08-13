16b01212282be62a6db200d0da751769debdf301
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-13 16:19:31 +0700

    feat: make landing pack Event and added tailwind css and set router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router"
 import Navbar from "./components/Navbar"
 import Footer from "./components/Footer"
+import Event from "./pages/Event"
 
 export default function Router() {
   return (
     <>
+    <Navbar/>
     <Routes>
-      <Route path={'/'} element={<Navbar/>}/>
+      <Route path={'/'} element={<Event/>}/>
     </Routes>
     <Footer/>
     </>
---
+++ src/pages/Event.jsx
@@ -0,0 +1,41 @@
+import search from '../assets/search.svg'
+import filter from '../assets/filter.svg'
+
+export default function Event() {
+  return (
+    <>
+    <header className="veryCenter px-14 py-5 shadow-xl gap-4">
+        <label className='veryCenter w-full bg-borderClr p-2 rounded-2xl'>
+            <img src={search} alt="search" className='h-7 w-7'/>
+            <input className='veryCenter px-2 py-1 outline-none w-full'
+            type="text" placeholder='Search events...'/>
+        </label>
+        <div className='veryCenter gap-2 myBorder hover:hover hover:border-orangeFigma'>
+            <img src={filter} alt="filter" className='w-5 h-5'/>
+            <div>Filters</div>
+        </div>
+    </header>
+    <main className="mainEvent">
+        <div>
+            <span className='font-bold'>10 </span><span>events found</span>
+        </div>
+        <div className='grid grid-cols-3 gap-3'>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>
+            <div>a</div>            
+        </div>
+        <div className='px-150'>
+            <div className='myBorder veryCenter hover:hover hover:border-orangeFigma'>
+                Load more events
+            </div>
+        </div>
+    </main>
+    </>
+  )
+}
