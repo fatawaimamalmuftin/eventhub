c1805645d44aa6e1f55f57a36a91a7f87fc15db2
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-13 17:01:06 +0700

    doc: added dummy data

--- src/lib/dummy.js
+++ src/lib/dummy.js
@@ -1,4 +1,4 @@
-const events = [
+const Events = [
   {
     id: 1,
     title: "Go Concurrency Workshop",
@@ -35,6 +35,90 @@ const events = [
     attendees: 32,
     capacity: 80,
   },
+  {
+    id: 4,
+    title: "JavaScript Developer Meetup",
+    image:
+      "https://images.unsplash.com/photo-1516116216624-53e697fedbea",
+    categories: ["Technology", "JavaScript"],
+    date: "Sep 2, 2026",
+    time: "09:30 WIB",
+    location: "Depok",
+    attendees: 64,
+    capacity: 100,
+  },
+  {
+    id: 5,
+    title: "Frontend Development Bootcamp",
+    image:
+      "https://images.unsplash.com/photo-1498050108023-c5249f4df085",
+    categories: ["Technology", "Frontend"],
+    date: "Sep 5, 2026",
+    time: "08:00 WIB",
+    location: "Jakarta",
+    attendees: 85,
+    capacity: 120,
+  },
+  {
+    id: 6,
+    title: "Backend API Workshop",
+    image:
+      "https://images.unsplash.com/photo-1558494949-ef010cbdcc31",
+    categories: ["Technology", "Backend"],
+    date: "Sep 8, 2026",
+    time: "10:00 WIB",
+    location: "Bekasi",
+    attendees: 41,
+    capacity: 70,
+  },
+  {
+    id: 7,
+    title: "Digital Marketing Summit",
+    image:
+      "https://images.unsplash.com/photo-1556761175-b413da4baf72",
+    categories: ["Business", "Marketing"],
+    date: "Sep 12, 2026",
+    time: "09:00 WIB",
+    location: "Jakarta",
+    attendees: 92,
+    capacity: 150,
+  },
+  {
+    id: 8,
+    title: "Photography Community Meetup",
+    image:
+      "https://images.unsplash.com/photo-1452780212940-6f5c0d14d848",
+    categories: ["Creative", "Photography"],
+    date: "Sep 15, 2026",
+    time: "14:00 WIB",
+    location: "Bandung",
+    attendees: 27,
+    capacity: 50,
+  },
+  {
+    id: 9,
+    title: "Startup Networking Night",
+    image:
+      "https://images.unsplash.com/photo-1556761175-5973dc0f32e7",
+    categories: ["Business", "Networking"],
+    date: "Sep 20, 2026",
+    time: "18:30 WIB",
+    location: "Tangerang",
+    attendees: 58,
+    capacity: 80,
+  },
+  {
+    id: 10,
+    title: "Cloud Computing Workshop",
+    image:
+      "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
+    categories: ["Technology", "Cloud"],
+    date: "Sep 25, 2026",
+    time: "09:00 WIB",
+    location: "Surabaya",
+    attendees: 36,
+    capacity: 60,
+  },
 ];
 
-export default events;
+export default Events;
