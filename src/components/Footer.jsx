4c8da335d53cc3a5b149aad1824362a6fda18fb5
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-13 15:24:26 +0700

    feat: make footer and custom tailwindcss

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+
+export default function Footer() {
+  return (
+    <footer className="borderFooter">
+        <div className="veryCenter gap-4 ">
+            <div className="text-biruFigma bg-bgBiruFigma py-1 px-2 rounded-xl">Technology</div>
+            <div className="text-biruFigma bg-bgBiruFigma py-1 px-2 rounded-xl">Ai</div>
+            <div className="text-unguFigma bg-bgUnguFigma py-1 px-2 rounded-xl">Design</div>
+        </div>
+
+        <div className="font-bold text-4xl">
+            Ready to find your community?
+        </div>
+
+        <div className="text-trFooter px-115 text-center">
+            Join thousands of developers, designers, and makers in Indonesia's most active tech communities.
+        </div>
+        
+        <div className=" veryCenter gap-6">
+            <div className=" btnBordColor">Explore Events</div>
+            <div className="btnTr">Browse Communities</div>
+        </div>        
+    </footer>
+  )
+}
