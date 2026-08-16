d723a70ef01d90ad5873ac5f565161a91ef9b8cd
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-16 23:15:51 +0700

    feat: make modal for logout and see profile

---
+++ src/components/ModalLogout.jsx
@@ -0,0 +1,15 @@
+
+export default function ModalLogout({show, setShow}) {
+  return (
+    <main className={`flex flex-col gap-5 absolute bg-white border-2 border-black/30 top-9 right-8 px-5 py-2 rounded-[16px_0_16px_16px] w-50 ${!show && "hidden"}`} >
+        <div className="flex items-center justify-between border-b-2 border-gray-400 px-2">
+            <div className="font-bold text-lg">Info</div>
+            <div className="font-bold hover:text-red-500 cursor-pointer"
+            onClick={(e)=>{e.stopPropagation(), setShow(false)}}
+            >X</div>
+        </div>
+        <div className="veryCenter text-2xl text-orangeFigma hover:bg-gray-100 rounded-2xl cursor-pointer">My Profile</div>
+        <button className="btnBordColor font-semibold py-1 hover:bg-black/20 hover:text-red-500">Logout</button>
+    </main>
+  )
+}
