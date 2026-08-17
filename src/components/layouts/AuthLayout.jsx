137597ce5f4965ffcfd2a44cc14ac706163b077b
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-17 13:48:42 +0700

    feat: barier user has logind before or admin

--- src/components/layouts/AuthLayout.jsx
+++ src/components/layouts/AuthLayout.jsx
@@ -1,7 +1,21 @@
-import { Outlet } from "react-router"
+import { Outlet, useNavigate } from "react-router"
 import SideBarAuth from "../SideBarAuth"
+import { useEffect } from "react"
 
 export default function AuthLayout() {
+
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+    const user = localStorage.getItem("userLogind")
+    const admin = localStorage.getItem("admin")
+
+    if(user || admin){
+      navigate("/", {replace:true})
+      return
+    }
+  },[navigate])
+
   return (
     <main className="grid grid-cols-1 md:grid-cols-2 h-screen w-screen overflow-hidden">
       <SideBarAuth />
