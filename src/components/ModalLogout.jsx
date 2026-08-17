4691f67afadbd7e2cd436afb589128f31e1e7c7a
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-17 13:49:18 +0700

    refactor: refactor Ui modal

--- src/components/ModalLogout.jsx
+++ src/components/ModalLogout.jsx
@@ -20,7 +20,7 @@ export default function ModalLogout({show, setShow, admin = null, setAdmin = nul
             onClick={(e)=>{e.stopPropagation(), setShow(false)}}
             >X</div>
         </div>
-        <div className="veryCenter text-2xl text-orangeFigma hover:bg-gray-100 rounded-2xl cursor-pointer whitespace-nowrap">
+        <div className="veryCenter px-4 py-2 text-2xl text-orangeFigma hover:bg-gray-100 rounded-2xl cursor-pointer whitespace-nowrap">
             {admin? <div>Admin Dashboard</div> : <div>My Profile</div>}
         </div>
         <button className="btnBordColor font-semibold py-1 hover:bg-black/20 hover:text-red-500" 
