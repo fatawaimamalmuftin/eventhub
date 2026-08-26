1d4360f09a1552e4340b2f81e9cb007e5df74777
fatawaimamalmuftin <fatawaimamalmuftin17@gmail.com>
2026-08-26 13:48:34 +0700

    refactor: use state for custom hook

--- src/Hooks/UseGetItem.js
+++ src/Hooks/UseGetItem.js
@@ -1,4 +1,9 @@
-export default function UseGetItem(key, init) {
-  const results = JSON.parse(localStorage.getItem(key)||init)
-  return results  
+import { useState } from "react";
+
+export default function useGetItem(key, init) {
+    const [results] = useState(
+        JSON.parse(localStorage.getItem(key) || init)
+    );
+
+    return results;
 }
